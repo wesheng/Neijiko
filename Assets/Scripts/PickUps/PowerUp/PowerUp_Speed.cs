d3a41e08af0a7c4c2f71b66450a541df146969d1
Sen <keima.ayasaki@gmail.com>
2018-08-15 10:10:20 -0600

    Added logic for power ups

---
+++ Assets/Scripts/PickUps/PowerUp/PowerUp_Speed.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUp_Speed : PowerUp_Base
+{
+
+    [SerializeField] private float duration;
+    [SerializeField] private float speedAddition;
+
+    public override void OnCollisionEnter(Collision hit)
+    {
+        Debug.Log(hit.gameObject.name);
+    }
+}

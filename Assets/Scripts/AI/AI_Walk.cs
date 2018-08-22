70c5cd8a1af0ca9808055142331dcbfe9da00a2d
Sen <keima.ayasaki@gmail.com>
2018-08-22 10:17:06 -0600

    Added code to make them walk

---
+++ Assets/Scripts/AI/AI_Walk.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AI_Walk : MonoBehaviour {
+    
+    [SerializeField] [Range(1.0f, 10.0f)] float m_speed = 1.0f;
+    [HideInInspector] public Animator m_animator = null;
+
+    [SerializeField] Vector3 waypointOffsetLeft     = new Vector3(-2.0f, 0.0f, 0.0f);
+    [SerializeField] Vector3 waypointOffsetRight    = new Vector3(+2.0f, 0.0f, 0.0f);
+    Vector3 currWaypoint;
+    [SerializeField] string currWaypointString = "ええええええええ";
+    
+    // Use this for initialization
+    void Start ()
+    {
+        waypointOffsetLeft.y = transform.position.y;
+        waypointOffsetRight.y = transform.position.y;
+        waypointOffsetLeft.z = transform.position.z;
+        waypointOffsetRight.z = transform.position.z;
+
+        currWaypoint = waypointOffsetLeft;
+        currWaypointString = "Left";
+        m_animator = GetComponent<Animator>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        Vector3 velocity = Vector3.zero;
+        Vector3 direction = Vector3.zero;
+
+        if (currWaypoint != null)
+        {
+            direction = currWaypoint - transform.position;
+            velocity.z = m_speed;
+        }
+
+        bool isWalking = !Mathf.Approximately(velocity.magnitude, 0.0f);
+        m_animator.SetBool("walk", isWalking);
+
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(direction, Vector3.up), Time.deltaTime);
+
+        velocity = transform.rotation * velocity;
+        transform.position = transform.position + (velocity * Time.deltaTime);
+
+        float distance = Mathf.Sqrt(Vector3.Dot(currWaypoint, transform.position));
+        if(transform.position.x < waypointOffsetLeft.x)
+        {
+            currWaypoint = waypointOffsetRight;
+        }
+        else if (transform.position.x > waypointOffsetRight.x)
+        {
+            currWaypoint = waypointOffsetLeft;
+        }
+    }
+}
